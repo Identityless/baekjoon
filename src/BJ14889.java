c4a7699c81ced96edf8393281ebb03c1f1ee67f7
Identityless <rlandwnd555@naver.com>
2023-08-26 17:19:11 +0900

    오류수정

--- src/BJ14889.java
+++ src/BJ14889.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class BJ14889 {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BJ14889 {
+    private static int N;
+    private static int[][] table;
+    private static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        table = new int[N][];
+
+        for (int i = 0 ; i < N ; i++) {
+            table[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        find(new ArrayList<>(), 0);
+
+        bw.write(min+"");
+        bw.flush();
+        bw.close();
+    }
+
+    private static void find(List<Integer> team1, int sp) {
+        if (N/2 - team1.size() > N - sp)
+            return;
+        if (team1.size() == N/2) {
+            min = Math.min(min, processBalance(team1));
+            return;
+        }
+        for (int i = sp ; i < N ; i++) {
+            if (!team1.contains(i)) {
+                team1.add(i);
+                find(team1, i);
+                team1.remove(team1.size()-1);
+            }
+        }
+    }
+
+    private static int processBalance(List<Integer> team1) {
+        List<Integer> team2 = new ArrayList<>();
+        int power1 = 0, power2 = 0;
+        for (int i = 0 ; i < N ; i++) {
+            if (!team1.contains(i)) {
+                team2.add(i);
+            }
+        }
+        for (int i = 0 ; i < N/2 ; i++) {
+            for (int j = i ; j < N/2 ; j++) {
+                power1 += table[team1.get(i)][team1.get(j)];
+                power1 += table[team1.get(j)][team1.get(i)];
+                power2 += table[team2.get(i)][team2.get(j)];
+                power2 += table[team2.get(j)][team2.get(i)];
+            }
+        }
+        return Math.abs(power1 - power2);
+    }
 }
--- src/BJ1912.java
+++ src/BJ1912.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class BJ1912 {
+import java.io.*;
+import java.util.Arrays;
+
+public class BJ1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] sums = new int[N];
+        int max = nums[0];
+        sums[0] = nums[0];
+
+        for (int i = 1 ; i < N ; i++) {
+            sums[i] = Math.max(nums[i], sums[i-1]+nums[i]);
+            if (sums[i] > max)
+                max = sums[i];
+        }
+
+        bw.write(max+"");
+        bw.flush();
+        bw.close();
+
+    }
 }
